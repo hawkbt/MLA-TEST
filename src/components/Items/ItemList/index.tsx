c2e7b895f0cab579e98d5b6a12ac8624e14e32d5
Jaseb Meza <jmeza792@gmail.com>
2025-06-07 00:59:07 -0300

    adding missing files and styles

--- src/components/Items/ItemList/index.tsx
+++ src/components/Items/ItemList/index.tsx
@@ -2,15 +2,16 @@
 import { SearchContext } from "@/context/searchContext";
 import React, { useContext } from "react";
 import Item from "../Item";
+import "./list.scss";
 
 const ItemList = () => {
   const { searchData } = useContext(SearchContext);
   return (
-    <>
+    <div className='list-wrapper'>
       {searchData?.map((item: SearchItem) => (
         <Item key={item.id} {...item} />
       ))}
-    </>
+    </div>
   );
 };
 
