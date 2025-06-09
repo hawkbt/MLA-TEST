9df3504ef3ca5d4f8f441ed34e46330a7929ff69
Jaseb Meza <jmeza792@gmail.com>
2025-06-08 23:35:53 -0300

    tooltip

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,9 +1,10 @@
 "use client";
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import "./searchBar.scss";
 import Image from "next/image";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { SearchContext } from "@/context/searchContext";
+import Tooltip from "../Tooltip";
 
 type FormValues = {
   search: string;
@@ -14,22 +15,30 @@ const SearchBar = () => {
   const { register, handleSubmit } = useForm<FormValues>({
     defaultValues: { search: searchValue ?? "" },
   });
+  const searchBarRef = useRef<HTMLDivElement>(null);
   const onSubmit: SubmitHandler<FormValues> = (formData) => triggerSearchData({ value: formData.search, offset: 0 });
 
   return (
-    <div className='search-bar' tabIndex={0}>
-      <form onSubmit={handleSubmit(onSubmit)} role='search' className='search-bar__content'>
-        <label htmlFor='search-input' className='search-bar__content__label visually-hidden'>
-          Ingresa lo que quieres Buscar
-        </label>
-        <input type='text' {...register("search")} className='search-bar__content__input' placeholder='Buscar productos, marcas y más...' />
-        <button type='submit' className='search-bar__content__button'>
-          <div className='search-bar__content__button__icon' aria-label='Buscar'>
-            <Image src='/images/search_icon@2x.png' alt='search icon' width={20} height={20} layout='intrinsic' />
-          </div>
-        </button>
-      </form>
-    </div>
+    <>
+      <div className='search-bar' tabIndex={0} ref={searchBarRef}>
+        <form onSubmit={handleSubmit(onSubmit)} role='search' className='search-bar__content'>
+          <label htmlFor='search-input' className='search-bar__content__label visually-hidden'>
+            Ingresa lo que quieres Buscar
+          </label>
+          <input type='text' {...register("search")} className='search-bar__content__input' placeholder='Buscar productos, marcas y más...' />
+          <button type='submit' className='search-bar__content__button'>
+            <div className='search-bar__content__button__icon' aria-label='Buscar'>
+              <Image src='/images/search_icon@2x.png' alt='search icon' width={20} height={20} layout='intrinsic' />
+            </div>
+          </button>
+        </form>
+      </div>
+      <Tooltip
+        targetRef={searchBarRef}
+        content={<p>Para realizar busquedas, solo debes ingresar el nombre de lo que necesites. Pueden ser productos, marcas y mas... </p>}
+        position='bottom'
+      />
+    </>
   );
 };
 
---
+++ src/components/Tooltip/index.tsx
@@ -0,0 +1,69 @@
+"use client";
+import React, { ReactNode, useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import "./tooltip.scss";
+type TooltipProps = {
+  targetRef: React.RefObject<HTMLElement | null>;
+  content: ReactNode | string;
+  position?: "top" | "right" | "bottom" | "left";
+};
+
+const Tooltip = (props: TooltipProps) => {
+  const { content, position = "top", targetRef } = props;
+  const firstTimeVisit = localStorage.getItem("firstTimeVisit");
+  const [visible, setVisible] = useState(JSON.parse(firstTimeVisit ?? "true"));
+  const [style, setStyle] = useState<React.CSSProperties>({});
+
+  const handleClose = () => {
+    localStorage.setItem("firstTimeVisit", "true");
+    setVisible(false);
+  };
+
+  useEffect(() => {
+    if (targetRef.current && visible) {
+      const rect = targetRef.current.getBoundingClientRect();
+
+      const tooltipStyles: { [key: string]: React.CSSProperties } = {
+        top: {
+          left: rect.left + rect.width / 2,
+          top: rect.top - 8,
+          transform: "translate(-50%, -100%)",
+        },
+        right: {
+          left: rect.right + 8,
+          top: rect.top + rect.height / 2,
+          transform: "translate(0, -50%)",
+        },
+        bottom: {
+          right: rect.width / 2,
+          top: rect.bottom + 4,
+          transform: "translate(-50%, 0)",
+        },
+        left: {
+          left: rect.left - 8,
+          top: rect.top + rect.height / 2,
+          transform: "translate(-100%, -50%)",
+        },
+      };
+
+      setStyle({
+        ...tooltipStyles[position],
+      });
+    }
+  }, [targetRef, visible, position]);
+
+  if (!visible) return null;
+
+  return ReactDOM.createPortal(
+    <div className={`tooltip-wrapper__box tooltip-${position}`} style={style} onClick={handleClose}>
+      <div className='tooltip-wrapper__box__close'>
+        <div className='tooltip-wraper__box__close-salut'>Hola</div>
+        <div className='tooltip-wraper__box__close-icon'>X</div>
+      </div>
+      {content}
+    </div>,
+    document.body
+  );
+};
+
+export default Tooltip;
